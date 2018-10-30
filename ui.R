3cf14b05b1c27319fdaf9b50b6abd91c514ceb45
cbardsley1793 <35579876+cbardsley1793@users.noreply.github.com>
2018-10-30 13:17:45 -0600

    Add files via upload

--- ui.R
+++ ui.R
@@ -1,15 +1,15 @@
 library(shiny)
 
 shinyUI(fluidPage(
-        titlePanel("Predict Old Faithful Geyser Eruption Duration Based on Wait Time"),
+        titlePanel("Old Faithful Geyser Eruption Duration Prediction App"),
         sidebarLayout(
                 sidebarPanel(
-                        sliderInput("sliderWT", "Wait Time to Next Old Faithful Eruption (in Minutes)", 50, 100, value = 75)
+                        sliderInput("sliderWT", "Move the slider to select a wait time (in mins) for next Old Faithful geyser eruption", 50, 100, value = 75)
                         # h4("Predicted Eruption Duration (in Minutes):"),
                         # textOutput("pred1")
                 ),
                 mainPanel(plotOutput("plot1"),
-                          h4("Predicted Eruption Duration (in Minutes):"),
+                          h4("The predicted eruption duration (in mins) based on the wait time selected is:"),
                           textOutput("pred1")
                 )
         )
